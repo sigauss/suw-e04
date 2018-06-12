bbf72ebfe4638a445aab3a24fc9323671c1f6794
clarzou <clairezunda@gmail.com>
2018-06-12 14:37:09 +0200

    try prod

--- store/index.js
+++ store/index.js
@@ -82,46 +82,46 @@ const createStore = () => {
       }
     },
     actions: {
-      nuxtServerInit({ commit }, { req }) {
-        console.log('NUXT SERVER INIT')
-        console.log(port)
-        let user = {};
-        if (req.session && req.session.authUser) {
-          user.username = req.session.authUser.username;
-          user.access_token = req.session.authUser.access_token;
-        } else {
-          user.username = "";
-          user.access_token = "";
-        }
-        if (req.session && req.session.activeRepo){
-          commit("SET_ACTIVEREPO", req.session.activeRepo);
-        }
-        if (req.session && req.session.activeCategory){
-          commit("SET_ACTIVECATEGORY", req.session.active_category);
-        }
-        return axios
-          .post(`${baseUrl}/api/login`, {
-            user
-          })
-          .then(res => {
-            if (req.session.authUser) {
-              commit("SET_USER", "logged");
-              return axios
-                .get(
-                  `${baseUrl}/api/github/user/${
-                    req.session.authUser.access_token
-                  }`
-                )
-                .then(res => {
-                  commit("SET_USERINFORMATIONS", res.data);
-                  commit("SET_ACCESSTOKEN", req.session.authUser.access_token);
-                })
-                .catch(error => {
-                  console.log(error.response);
-                });
-            }
-          });
-      },
+      // nuxtServerInit({ commit }, { req }) {
+      //   console.log('NUXT SERVER INIT')
+      //   console.log(port)
+      //   let user = {};
+      //   if (req.session && req.session.authUser) {
+      //     user.username = req.session.authUser.username;
+      //     user.access_token = req.session.authUser.access_token;
+      //   } else {
+      //     user.username = "";
+      //     user.access_token = "";
+      //   }
+      //   if (req.session && req.session.activeRepo){
+      //     commit("SET_ACTIVEREPO", req.session.activeRepo);
+      //   }
+      //   if (req.session && req.session.activeCategory){
+      //     commit("SET_ACTIVECATEGORY", req.session.active_category);
+      //   }
+      //   return axios
+      //     .post(`${baseUrl}/api/login`, {
+      //       user
+      //     })
+      //     .then(res => {
+      //       if (req.session.authUser) {
+      //         commit("SET_USER", "logged");
+      //         return axios
+      //           .get(
+      //             `${baseUrl}/api/github/user/${
+      //               req.session.authUser.access_token
+      //             }`
+      //           )
+      //           .then(res => {
+      //             commit("SET_USERINFORMATIONS", res.data);
+      //             commit("SET_ACCESSTOKEN", req.session.authUser.access_token);
+      //           })
+      //           .catch(error => {
+      //             console.log(error.response);
+      //           });
+      //       }
+      //     });
+      // },
       setAccessToken({ commit }, access_token) {
         commit("SET_ACCESSTOKEN", access_token);
       },
