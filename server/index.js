47a8584470ee3bec9102755bc74022e5dfdeac29
clarzou <clairezunda@gmail.com>
2018-06-14 22:49:08 +0200

    use mongo session

--- server/index.js
+++ server/index.js
@@ -4,15 +4,29 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const MongoStore = require('connect-mongo')(session);
 
 let port = process.env.PORT || 3000
 let host = process.env.HOST || '127.0.0.1'
+
+const mongoose = require('mongoose');
+ 
+//connect to MongoDB
+mongoose.connect('mongodb://ds147450.mlab.com:47450/suw-accio', {user: 'arnaud', pass: 'suwe04'});
+const db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('connected')
+});
 
 app.use(session({
   secret: 'super-secret-key',
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 60000 }
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 
 app.use(bodyParser.json())
